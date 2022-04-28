abd7d61daca483c28957fc24f87f3704a7b8e895
v3.3v33 <vvv333dev@gmail.com>
2022-04-28 23:03:24 +0300

    Added unit tests for exceptions

--- tests/unit/test_payment_client.py
+++ tests/unit/test_payment_client.py
@@ -6,6 +6,7 @@ from requests import Session
 
 from dhf_wrapper.client import PaymentClient
 from dhf_wrapper.entities.payment import PaymentDTO
+from dhf_wrapper.exceptions import DHFUserUnauthorized, DHFBadRequest, DHFMethodNotFound
 
 
 class TestPaymentClient(TestCase):
@@ -34,11 +35,64 @@ class TestPaymentClient(TestCase):
         mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=payments), json=lambda: payments)
         mock_get.return_value = mocked_session
 
-        payment_client = PaymentClient('http://example.com', token='xxxxx')
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
 
         response = payment_client.get_payments()
 
         self.assertEqual(response, payments)
+
+    @patch.object(Session, 'get')
+    def test_negative_getting_payments_400_handling(self, mock_get):
+        payments_400 = {"statusCode": 400,
+                        "message": "Cannot GET /api/transactionsd",
+                        "error": "Not Found"
+                        }
+
+        patch.object(Session, 'get')
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=payments_400),
+                                                               json=lambda: payments_400)
+        mock_get.return_value = mocked_session
+
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFBadRequest) as e:
+            client_response = payment_client.get_payments()
+
+    @patch.object(Session, 'get')
+    def test_negative_getting_payments_401_handling(self, mock_get):
+        payments_401 = {"statusCode": 401,
+                        "message": "Cannot GET /api/paymentss",
+                        "error": "Not Found"
+                        }
+
+        patch.object(Session, 'get')
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=payments_401),
+                                                               json=lambda: payments_401)
+        mock_get.return_value = mocked_session
+
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFUserUnauthorized) as e:
+            client_response = payment_client.get_payments()
+
+    @patch.object(Session, 'get')
+    def test_negative_getting_payments_404_handling(self, mock_get):
+        payments_404 = {"statusCode": 404,
+                        "message": "Cannot GET /api/transactionsd",
+                        "error": "Not Found"
+                        }
+        patch.object(Session, 'get')
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=payments_404),
+                                                               json=lambda: payments_404)
+        mock_get.return_value = mocked_session
+
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFMethodNotFound) as e:
+            client_response = payment_client.get_payments()
 
     @patch.object(Session, 'get')
     def test_positive_getting_payment(self, mock_get):
@@ -63,11 +117,62 @@ class TestPaymentClient(TestCase):
         mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=payment), json=lambda: payment)
         mock_get.return_value = mocked_session
 
-        payment_client = PaymentClient('http://example.com', token='xxxxx')
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
 
         response = payment_client.get_payment(payment_id=1)
 
         self.assertEqual(response, payment)
+
+    @patch.object(Session, 'get')
+    def test_negative_getting_payment_400_handling(self, mock_get):
+        payments_400 = {"statusCode": 400,
+                        "message": "Cannot GET /api/transactionsd/1",
+                        "error": "Not Found"
+                        }
+        patch.object(Session, 'get')
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=payments_400),
+                                                               json=lambda: payments_400)
+        mock_get.return_value = mocked_session
+
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFBadRequest) as e:
+            client_response = payment_client.get_payment(payment_id=1)
+
+    @patch.object(Session, 'get')
+    def test_negative_getting_payment_401_handling(self, mock_get):
+        payments_401 = {"statusCode": 401,
+                        "message": "Cannot GET /api/transactionsd",
+                        "error": "Not Found"
+                        }
+        patch.object(Session, 'get')
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=payments_401),
+                                                               json=lambda: payments_401)
+        mock_get.return_value = mocked_session
+
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFUserUnauthorized) as e:
+            client_response = payment_client.get_payment(payment_id=1)
+
+    @patch.object(Session, 'get')
+    def test_negative_getting_payment_404_handling(self, mock_get):
+        payments_404 = {"statusCode": 404,
+                        "message": "Cannot GET /api/transactionsd",
+                        "error": "Not Found"
+                        }
+        patch.object(Session, 'get')
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=payments_404),
+                                                               json=lambda: payments_404)
+        mock_get.return_value = mocked_session
+
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFMethodNotFound) as e:
+            client_response = payment_client.get_payment(payment_id=1)
 
     @patch.object(Session, 'post')
     def test_positive_create_payment(self, mock_post):
@@ -79,7 +184,7 @@ class TestPaymentClient(TestCase):
         mocked_session.__enter__.return_value = mock.MagicMock(post=mock.MagicMock(return_value=payment_id), json=lambda: payment_id)
         mock_post.return_value = mocked_session
 
-        payment_client = PaymentClient('http://example.com', token='xxxxx')
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
 
         response = payment_client.create_payment(payment=PaymentDTO(
             amount=1234,
@@ -98,7 +203,67 @@ class TestPaymentClient(TestCase):
         mocked_session.__enter__.return_value = mock.MagicMock(post=mock.MagicMock(return_value=payment_id), json=lambda: payment_id)
         mock_post.return_value = mocked_session
 
-        payment_client = PaymentClient('http://example.com', token='xxxxx')
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
 
         with self.assertRaises(TypeError) as e:
             payment_client.create_payment()
+
+    @patch.object(Session, 'post')
+    def test_negative_create_payment_400_handling(self, mock_post):
+        payment_400 = {
+            "statusCode": 400,
+            "message": [
+                "amount must not be less than 2500000000"
+            ],
+            "error": "Bad Request"
+        }
+
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(post=mock.MagicMock(return_value=payment_400), json=lambda: payment_400)
+        mock_post.return_value = mocked_session
+
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFBadRequest) as e:
+            payment_client.create_payment(payment=PaymentDTO(
+                amount=1234,
+                comment="test"
+            ))
+
+    @patch.object(Session, 'post')
+    def test_negative_create_payment_401_handling(self, mock_post):
+        payment_401 = {"statusCode": 401,
+                        "message": "Cannot GET /api/transactionsd",
+                        "error": "Not Found"
+                        }
+
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(post=mock.MagicMock(return_value=payment_401), json=lambda: payment_401)
+        mock_post.return_value = mocked_session
+
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFUserUnauthorized) as e:
+            payment_client.create_payment(payment=PaymentDTO(
+                amount=1234,
+                comment="test"
+            ))
+
+    @patch.object(Session, 'post')
+    def test_negative_create_payment_404_handling(self, mock_post):
+        payment_404 = {"statusCode": 404,
+                        "message": "Cannot GET /api/transactionsd",
+                        "error": "Not Found"
+                        }
+
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(post=mock.MagicMock(return_value=payment_404), json=lambda: payment_404)
+        mock_post.return_value = mocked_session
+
+        payment_client = PaymentClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFMethodNotFound) as e:
+            payment_client.create_payment(payment=PaymentDTO(
+                amount=1234,
+                comment="test"
+            ))
--- tests/unit/test_transaction_client.py
+++ tests/unit/test_transaction_client.py
@@ -6,6 +6,7 @@ from requests import Session
 
 from dhf_wrapper.client import TransactionClient
 from dhf_wrapper.entities.transaction import TransactionParamsDTO
+from dhf_wrapper.exceptions import DHFUserUnauthorized, DHFBadRequest, DHFMethodNotFound
 
 
 class TestTransactionClient(TestCase):
@@ -33,7 +34,7 @@ class TestTransactionClient(TestCase):
         mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=transactions), json=lambda: transactions)
         mock_get.return_value = mocked_session
 
-        transaction_client = TransactionClient('http://example.com', token='xxxxx')
+        transaction_client = TransactionClient(base_url='http://example.com', token='xxxxx')
 
         response = transaction_client.get_transactions(
             params=TransactionParamsDTO(limit=1)
@@ -63,9 +64,65 @@ class TestTransactionClient(TestCase):
 
         mock_get.side_effect = ConnectionError()
 
-        transaction_client = TransactionClient('http://example.com', token='xxxxx')
+        transaction_client = TransactionClient(base_url='http://example.com', token='xxxxx')
 
         with self.assertRaises(ConnectionError) as e:
+            transaction_client.get_transactions(
+                params=TransactionParamsDTO(limit=1)
+            )
+
+    @patch.object(Session, 'get')
+    def test_negative_getting_transactions_400_handling(self, mock_get):
+        transactions_400 = {
+            "statusCode": 400,
+            "message": [
+                "Some transaction getting error"
+            ],
+            "error": "Bad Request"
+        }
+
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=transactions_400), json=lambda: transactions_400)
+        mock_get.return_value = mocked_session
+        transaction_client = TransactionClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFBadRequest) as e:
+            transaction_client.get_transactions(
+                params=TransactionParamsDTO(limit=1)
+            )
+
+    @patch.object(Session, 'get')
+    def test_negative_getting_transactions_401_handling(self, mock_get):
+        transactions_401 = {
+            "statusCode": 401,
+            "message": "Cannot GET /api/transactions",
+            "error": "Bad token"
+        }
+
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=transactions_401), json=lambda: transactions_401)
+        mock_get.return_value = mocked_session
+        transaction_client = TransactionClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFUserUnauthorized) as e:
+            transaction_client.get_transactions(
+                params=TransactionParamsDTO(limit=1)
+            )
+
+    @patch.object(Session, 'get')
+    def test_negative_getting_transactions_404_handling(self, mock_get):
+        transactions_404 = {
+            "statusCode": 404,
+            "message": "Cannot GET /api/transactionss",
+            "error": "Not Found"
+        }
+
+        mocked_session = mock.MagicMock()
+        mocked_session.__enter__.return_value = mock.MagicMock(get=mock.MagicMock(return_value=transactions_404), json=lambda: transactions_404)
+        mock_get.return_value = mocked_session
+        transaction_client = TransactionClient(base_url='http://example.com', token='xxxxx')
+
+        with self.assertRaises(DHFMethodNotFound) as e:
             transaction_client.get_transactions(
                 params=TransactionParamsDTO(limit=1)
             )
